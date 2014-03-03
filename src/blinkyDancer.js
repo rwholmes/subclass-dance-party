aabed67e83afa80200e62969873efe3d92c8def3
Robert Holmes <rwholmes1@gmail.com>
2014-03-03 15:07:33 -0800

    Working blinky dancer

--- src/blinkyDancer.js
+++ src/blinkyDancer.js
@@ -1,12 +1,13 @@
 var BlinkyDancer = function(top, left, timeBetweenSteps){
   Dancer.apply(this, arguments);
-  //this._timeBetweenSteps = timeBetweenSteps;
   // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
   // so we must keep a copy of the old version of this function
 
 };
 
 BlinkyDancer.prototype = Object.create(Dancer.prototype);  
+// BlinkyDancer.prototype = new Dancer();
+BlinkyDancer.prototype.constructor = BlinkyDancer;
 
 BlinkyDancer.prototype.step = function(){
   // call the old version of step at the beginning of any call to this new version of step
